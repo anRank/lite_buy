65bb9af3278df3012f54fb1460322805ad524890
suchang <1103232282@qq.com>
2020-03-26 17:50:27 +0800

    copy

--- buy_api/app/controller/__init__.py
+++ buy_api/app/controller/__init__.py
@@ -5,35 +5,12 @@ from easyapi import EasyApiContext
 
 
 
-TYPE_MAPPING = {
-    0: '租赁',
-    1: '采购'
-}
-
-TYPE_MAPPING_REV = {v:k for k, v in TYPE_MAPPING.items()}
-
-
 class KindController(easyapi.BaseController):
     __dao__ = dao.KindDao
 
 
 class CompanyController(easyapi.BaseController):
     __dao__ = dao.CompanyDao
-
-    #formatter是out   reformatter是in 进入db
-    @classmethod
-    def formatter(cls, ctx: EasyApiContext , data: dict):
-        data['production_kind'] = [k[1:-1] for k in  data.get('production_kind', '').split(',')]
-        data['type'] = TYPE_MAPPING[data.get('type', 0)] 
-        return data
-
-    @classmethod
-    def reformatter(cls, ctx: EasyApiContext , data: dict):
-        if 'production_kind' in data:
-            data['production_kind'] = ','.join('|{}|'.format(k) for k in   data['production_kind'])
-        if 'type' in data:
-            data['type'] = TYPE_MAPPING_REV[data['type']]
-        return data
 
 
 class ResultController(easyapi.BaseController):
--- buy_api/app/dao/__init__.py
+++ buy_api/app/dao/__init__.py
@@ -1,9 +1,37 @@
 import easyapi
 from app.db import sqlite_db
+from easyapi import EasyApiContext
+import copy
+TYPE_MAPPING = {
+    0: '租赁',
+    1: '采购'
+}
+
+TYPE_MAPPING_REV = {v:k for k, v in TYPE_MAPPING.items()}
+
 
 class CompanyDao(easyapi.BusinessBaseDao):
     __tablename__ = 'companys'
     __db__ = sqlite_db
+    
+    @classmethod
+    def formatter(cls, ctx: EasyApiContext , data: dict):
+        data = super().formatter(ctx, data)
+        new_data = {k:v for k, v in data.items()}
+        new_data['production_kind'] = [k[1:-1] for k in  new_data.get('production_kind', '').split(',')]
+        new_data['type'] = TYPE_MAPPING[data.get('type', 0)] 
+        return new_data
+
+    @classmethod
+    def reformatter(cls, ctx: EasyApiContext , data: dict):
+        new_data = super().reformatter(ctx, data)
+        new_data = {k:v for k, v in new_data.items()}
+        if 'production_kind' in new_data:
+            new_data['production_kind'] = ','.join('|{}|'.format(k) for k in   new_data['production_kind'])
+        if 'type' in data:
+            new_data['type'] = TYPE_MAPPING_REV[new_data['type']]
+        return new_data
+
     
 
 class KindDao(easyapi.BusinessBaseDao):
